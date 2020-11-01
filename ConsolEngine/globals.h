12e52c197d4e1dd134e2e90c268f64452a79c50f
Stanislaw <dr.radov2010@gmail.com>
2020-11-01 22:02:23 +0200

    Added global constants

---
+++ ConsolEngine/globals.h
@@ -0,0 +1,13 @@
+#ifndef __GLOBALS_H__
+#define __GLOBALS_H__
+
+const int TETROMINO_SIZE = 4;//number of bricks in one figure
+const int FIELD_HEIGHT = 22;//height of the game's field
+const int FIELD_WIDTH = 15;//width of the game's field
+const int leftBorder = 1; //left border of the game
+const int rightBorder = 16; //right border of the game
+const int bottomBorder = 22;//bottom of the game
+const int averageSpeed = 20;//average speed of falling
+const int uncontrollableSpeed = 2;//speed of uncontrollable's figure falling
+
+#endif  __GLOBALS_H__
